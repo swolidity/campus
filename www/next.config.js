6b90554665d560589873f7f54fb193e263143a26
Andy Kay <andykay.me@gmail.com>
2019-08-06 03:56:07 -0400

    first query working! 💯

--- www/next.config.js
+++ www/next.config.js
@@ -3,6 +3,8 @@ const prod = process.env.NODE_ENV === "production";
 module.exports = {
   target: "serverless",
   env: {
-    API_URL: prod ? "https://better.now.sh/api" : "http://localhost:4000"
+    API_URL: prod
+      ? "https://better.now.sh/api/graphql"
+      : "http://localhost:3000/api/graphql"
   }
 };
--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,1 +1,20 @@
-export default () => <div>This is Better!</div>;
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+const GET_USERS = gql`
+  query getUsers {
+    findManyUser {
+      id
+      name
+      email
+      createdAt
+    }
+  }
+`;
+
+export default () => {
+  const { loading, data } = useQuery(GET_USERS);
+
+  console.log(data);
+  return <div>This is Better!</div>;
+};
