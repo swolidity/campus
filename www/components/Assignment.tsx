e22f49220d5eb7336ba2482ec04c08948e4808df
Andy Kay <andykay.me@gmail.com>
2019-11-05 17:44:30 -0500

    initial performance optimizations with memoization

--- www/components/Assignment.tsx
+++ www/components/Assignment.tsx
@@ -20,6 +20,7 @@ import {
   Button
 } from "@chakra-ui/core";
 import { useState, useEffect, useCallback } from "react";
+import GradeRow from "../components/GradeRow";
 
 const GET_ASSIGNMENT = gql`
   query GET_ASSIGNMENT($where: AssignmentWhereUniqueInput!) {
@@ -57,9 +58,24 @@ export default function Assignment() {
 
   const [grades, setGrades] = useState({});
 
+  const handlePointsChange = useCallback(
+    number => user => {
+      console.log("handlePointsChange");
+      setGrades(grades => ({
+        ...grades,
+        [user.id]: {
+          ...grades[user.id],
+          points: number
+        }
+      }));
+    },
+    []
+  );
+
   if (loading) return <div>Loading...</div>;
 
   useEffect(() => {
+    console.log("useEffect");
     if (data.assignment.course.users) {
       const users = data.assignment.course.users;
 
@@ -103,47 +119,12 @@ export default function Assignment() {
 
       <Stack spacing={2}>
         {data.assignment.course.users.map(user => (
-          <Box shadow="sm" p={2} key={user.id}>
-            <Flex align="center" mb={2}>
-              <Avatar src={user.picture} name={user.name} mr={2} size="sm" />
-              <Text>{user.name}</Text>
-            </Flex>
-
-            <Flex align="center">
-              <Select
-                aria-labelledby="Letter Grade"
-                placeholder="Letter Grade"
-              ></Select>
-
-              <NumberInput
-                defaultValue={0}
-                precision={2}
-                step={1}
-                mr={2}
-                value={grades[user.id].points}
-                id="points"
-                onChange={number => {
-                  setGrades({
-                    ...grades,
-                    [user.id]: {
-                      ...grades[user.id],
-                      points: number
-                    }
-                  });
-                }}
-              >
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-
-              <Text>Points</Text>
-
-              <Input placeholder="%" />
-            </Flex>
-          </Box>
+          <GradeRow
+            user={user}
+            points={grades[user.id].points}
+            onPointsChange={handlePointsChange}
+            key={user.id}
+          />
         ))}
       </Stack>
     </Box>
---
+++ www/components/GradeRow.tsx
@@ -0,0 +1,55 @@
+import { memo } from "react";
+import {
+  Flex,
+  Box,
+  Select,
+  Avatar,
+  Text,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Input
+} from "@chakra-ui/core";
+
+const GradeRow = ({ user, points, onPointsChange }) => {
+  console.log("GradeRow render");
+  return (
+    <Box shadow="sm" p={2}>
+      <Flex align="center" mb={2}>
+        <Avatar src={user.picture} name={user.name} mr={2} size="sm" />
+        <Text>{user.name}</Text>
+      </Flex>
+
+      <Flex align="center">
+        <Select
+          aria-labelledby="Letter Grade"
+          placeholder="Letter Grade"
+        ></Select>
+
+        <NumberInput
+          defaultValue={0}
+          precision={2}
+          step={1}
+          mr={2}
+          value={points}
+          id="points"
+          onChange={number => onPointsChange(number)(user)}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+
+        <Text>Points</Text>
+
+        <Input placeholder="%" />
+      </Flex>
+    </Box>
+  );
+};
+
+export default memo(GradeRow);
