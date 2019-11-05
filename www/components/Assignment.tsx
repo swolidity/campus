e621b4c714ace2ac79a74acc5af492f976f78fd1
Andy Kay <andykay.me@gmail.com>
2019-11-05 14:28:29 -0500

    initial gradebook manipulations

--- www/components/Assignment.tsx
+++ www/components/Assignment.tsx
@@ -45,7 +45,7 @@ const GET_ASSIGNMENT = gql`
   }
 `;
 
-export default function Course() {
+export default function Assignment() {
   const router = useRouter();
   const { loading, data } = useQuery(GET_ASSIGNMENT, {
     variables: {
@@ -55,14 +55,50 @@ export default function Course() {
     }
   });
 
+  const [grades, setGrades] = useState({});
+
   if (loading) return <div>Loading...</div>;
+
+  useEffect(() => {
+    if (data.assignment.course.users) {
+      const users = data.assignment.course.users;
+
+      const newGrades = setGrades(
+        Object.assign(
+          {},
+          ...users.map(user => ({
+            [user["id"]]: {
+              id: user.id,
+              letter_grade: null,
+              points: 0,
+              percent: 0
+            }
+          }))
+        )
+      );
+    }
+  }, [data.assignment.course.users]);
+
+  if (Object.keys(grades).length < 1) return null;
+
+  const setAllPointsTo = () => {
+    let updatedGrades = {};
+
+    for (const grade in grades) {
+      updatedGrades[grade] = { ...grades[grade], points: 10 };
+    }
+
+    setGrades(updatedGrades);
+  };
+
+  console.log(grades);
 
   return (
     <Box>
       <Heading mb={4}>{data.assignment.name}</Heading>
 
-      <Button color="green" mb={4}>
-        Save
+      <Button color="green" mb={4} onClick={setAllPointsTo}>
+        Set All Points To 10
       </Button>
 
       <Stack spacing={2}>
@@ -79,7 +115,23 @@ export default function Course() {
                 placeholder="Letter Grade"
               ></Select>
 
-              <NumberInput defaultValue={0} precision={2} step={1} mr={2}>
+              <NumberInput
+                defaultValue={0}
+                precision={2}
+                step={1}
+                mr={2}
+                value={grades[user.id].points}
+                id="points"
+                onChange={number => {
+                  setGrades({
+                    ...grades,
+                    [user.id]: {
+                      ...grades[user.id],
+                      points: number
+                    }
+                  });
+                }}
+              >
                 <NumberInputField />
                 <NumberInputStepper>
                   <NumberIncrementStepper />
